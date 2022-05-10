8c7cd02ba160e82f1f5c9dbfd0486a4491d8d279
AndreMinnie <andre@duple.co>
2022-05-10 21:44:27 +0200

    update: Changed to Nuxt2

---
+++ sspud-admin/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'sspud-admin',
+        exec_mode: 'cluster',
+        instances: '2', // Or a number of instances
+        script: './node_modules/nuxt/bin/nuxt.js',
+        args: 'start',
+      }
+    ]
+
+  }
