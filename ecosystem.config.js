d52e562ca051ca5dec26e4052150d595321cfaa3
MarkusAfricanus <marco.pieterse@gmail.com>
2022-05-11 16:34:04 +0200

    BAM-13: Added environments to the ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,12 @@ module.exports = {
         instances: '2', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start',
+        env_production: {
+            NODE_ENV: "production",
+        },
+        env_development: {
+            NODE_ENV: "development",
+        }
       }
     ]
-
   }
