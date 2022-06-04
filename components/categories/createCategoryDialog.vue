ef9e8f37c133281dc355cc33b1cf073cd8e14bd3
jacqxes-du <jacques@duple.co>
2022-06-04 12:17:06 +0200

    BAM-105: Updated the linked entities component on the product detail page

--- components/categories/createCategoryDialog.vue
+++ components/categories/createCategoryDialog.vue
@@ -1,69 +1,76 @@
 <template>
   <div>
     <button
-        @click="openDialog()"
-        type="button" class="btn btn-sm btn-outline-secondary">
+      @click="openDialog()"
+      type="button"
+      class="btn btn-sm btn-outline-secondary"
+    >
       New Category
     </button>
     <!--New Category Dialog-->
-    <v-dialog style="z-index: 10000" v-model="newCategoryDialog" max-width="800">
+    <v-dialog
+      style="z-index: 10000"
+      v-model="newCategoryDialog"
+      max-width="800"
+    >
       <v-card v-if="category">
-        <v-card-title>
-          New Category
-        </v-card-title>
-        <v-card-subtitle>
-          Create a new category
-        </v-card-subtitle>
+        <v-card-title> New Category </v-card-title>
+        <v-card-subtitle> Create a new category </v-card-subtitle>
         <v-card-text class="pb-0">
           <v-form
-              ref="newCategoryForm"
-              v-model="validCategoryForm"
-              lazy-validation>
+            ref="newCategoryForm"
+            v-model="validCategoryForm"
+            lazy-validation
+          >
             <v-text-field
-                prepend-icon="mdi-card-account-details-outline"
-                v-model="category.name"
-                :rules="[(v) => !!v || 'A Name is required']"
-                hint="The name of the category"
-                v-on:change="checkIfCategoryExists(category.name)"
-                label="Name"
+              prepend-icon="mdi-card-account-details-outline"
+              v-model="category.name"
+              :rules="[(v) => !!v || 'A Name is required']"
+              hint="The name of the category"
+              v-on:change="checkIfCategoryExists(category.name)"
+              label="Name"
             ></v-text-field>
-            <div
-             v-show="categoryExists"
-            >
-            <h5 style="color:red;">This category already exists! Please enter another category name.</h5>
+            <div v-show="categoryExists">
+              <h5 style="color: red">
+                This category already exists! Please enter another category
+                name.
+              </h5>
             </div>
             <v-select
-                prepend-icon="mdi-clipboard-check-multiple"
-                label="Parent"
-                :item-text="'name'"
-                :item-value="'id'"
-                :items="availableCategories"
-                v-model="category.parentId"
-                :messages="['Choose a parent or leave blank']"
+              prepend-icon="mdi-clipboard-check-multiple"
+              label="Parent"
+              :item-text="'name'"
+              :item-value="'id'"
+              :items="availableCategories"
+              v-model="category.parentId"
+              :messages="['Choose a parent or leave blank']"
             ></v-select>
           </v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-progress-circular
-              v-if="loading"
-              :size="20"
-              indeterminate
-              color="primary"
+            v-if="loading"
+            :size="20"
+            indeterminate
+            color="primary"
           ></v-progress-circular>
           <v-btn
-              v-if="!loading"
-              :disabled="categoryExists"
-              color="primary"
-              text
-              v-on:click="saveCategory">
+            v-if="!loading"
+            :disabled="categoryExists"
+            color="primary"
+            text
+            v-on:click="saveCategory"
+          >
             Save
           </v-btn>
           <v-btn
-              :disabled="loading"
-              color="primary"
-              text
-              @click="newCategoryDialog = false">Close</v-btn>
+            :disabled="loading"
+            color="primary"
+            text
+            @click="newCategoryDialog = false"
+            >Close</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -75,7 +82,7 @@ export default {
   props: {
     categories: [],
     saveCallBack: null,
-    allCategories: []
+    allCategories: [],
   },
   data() {
     return {
@@ -85,71 +92,74 @@ export default {
       category: null,
       categoryExists: false,
       subCategoryExists: false,
-      availableCategories: []
-    }
+      availableCategories: [],
+    };
   },
-  mounted() {
-
-  },
+  mounted() {},
   methods: {
     async openDialog() {
       this.newCategoryDialog = true;
 
-      this.allCategories.forEach(element => {
+      this.allCategories.forEach((element) => {
         // console.log(element);
 
         if (element.parentId === 0 || element.parentId == null) {
-            this.availableCategories.push(element);
+          this.availableCategories.push(element);
         }
-        
       });
 
       // Create new  default category
       this.category = {
-        name: '',
+        name: "",
         parentId: null,
-        publish: false
-      }
+        publish: false,
+      };
     },
     async saveCategory() {
       this.loading = true;
-     // Save the new category if the form is valid
+      // Save the new category if the form is valid
       if (this.$refs.newCategoryForm.validate()) {
-        const response = await this.$store.dispatch('dataGate', {
-          primaryKey: 'id',
+        const response = await this.$store.dispatch("dataGate", {
+          primaryKey: "id",
           entity: this.category,
-          tableName: 'mappedCategories',
-          operation: 'create',
-        })
+          tableName: "mappedCategories",
+          operation: "create",
+        });
         // If valid response return value
         if (response && response.response) {
-          this.saveCallBack(this.category)
+          this.saveCallBack(this.category);
+
+          this.category.id = response.insertId;
+          const responseInsertToWC = await this.$store.dispatch(
+            "callMiddlewareRoute",
+            {
+              category: this.category,
+              route: "categories/insertCategoryToWC",
+            }
+          );
         }
       }
       this.newCategoryDialog = false;
       this.loading = false;
     },
-    async checkIfCategoryExists(categoryName){
-        const catResponse = await this.$store.dispatch('dataGate', {
-            whereCriteria: {name: categoryName},
-            tableName: 'mappedCategories',
-            operation: 'read',
-        })
+    async checkIfCategoryExists(categoryName) {
+      const catResponse = await this.$store.dispatch("dataGate", {
+        whereCriteria: { name: categoryName },
+        tableName: "mappedCategories",
+        operation: "read",
+      });
 
-        console.log("👉👉", catResponse.data);
+      console.log("👉👉", catResponse.data);
 
-        if(catResponse.data.length == 0){
-          this.categoryExists = false;
-        }else{
-          this.categoryExists = true;
-        }
-
-
-    }
-  }
-}
+      if (catResponse.data.length == 0) {
+        this.categoryExists = false;
+      } else {
+        this.categoryExists = true;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
--- components/products/productLinkedEntities.vue
+++ components/products/productLinkedEntities.vue
@@ -1,100 +1,142 @@
 <template>
   <div class="product-publish-component card mt-10 mb-10">
     <div class="" v-if="product">
-      <v-alert
-          v-if="!product.publish"
-          border="right"
-          colored-border
-          type="error"
-          elevation="2"
-          class="info-message">
-        This Product is not published
-      </v-alert>
-      <v-alert
-          v-if="product.publish"
-          border="right"
-          colored-border
-          type="success"
-          elevation="2"
-          class="info-message">
-        This Product is  published
-      </v-alert>
-      <v-alert
-          v-if="product.BOBSRequired && !product.BOBSCertificate"
-          border="right"
-          colored-border
-          type="error"
-          elevation="2"
-          class="info-message">
-        BOBS Certificate has not been uploaded
-      </v-alert>
-      <v-alert
-          v-if="product.BOBSRequired && product.BOBSCertificate"
-          border="right"
-          colored-border
-          type="success"
-          elevation="2"
-          class="info-message">
-        BOBS Certificate has been uploaded
-      </v-alert>
-      <v-alert
-          v-if="!product.BOBSRequired"
-          border="right"
-          colored-border
-          type="success"
-          elevation="2"
-          class="info-message">
-        This Product does not require a BOBS Certificate
-      </v-alert>
-      <v-alert
-          v-if="product.SABSRequired && !product.SABSCertificate"
-          border="right"
-          colored-border
-          type="error"
-          elevation="2"
-          class="info-message">
-        SABS Certificate has not been uploaded
-      </v-alert>
-      <v-alert
-          v-if="product.SABSRequired && product.SABSCertificate"
-          border="right"
-          colored-border
-          type="success"
-          elevation="2"
-          class="info-message">
-        SABS Certificate has been uploaded
-      </v-alert>
-      <v-alert
-          v-if="!product.SABSRequired"
-          border="right"
-          colored-border
-          type="success"
-          elevation="2"
-          class="info-message">
-        This Product does not require an SABS Certificate
-      </v-alert>
+      <!-- BZ Main Category -->
+      <v-card class="info-message">
+        <v-card-title> BambaZonke Category </v-card-title>
+        <v-card-subtitle class="card-subtitle">
+          {{ getCategory(product.categoryId) }}
+        </v-card-subtitle>
+        <v-row>
+          <v-col cols="7">
+            <v-card-text>
+              <h6>BOBS Required:</h6>
+              <h6>SABS Required:</h6>
+              <h6>Published:</h6>
+            </v-card-text>
+          </v-col>
+          <v-col cols="5">
+            <v-card-text>
+              <h6>{{ catBOBS }}</h6>
+              <h6>{{ catSABS }}</h6>
+              <h6>{{ catPublished }}</h6>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <!-- BZ Subcategory -->
+      <v-card class="info-message">
+        <v-card-title> BambaZonke Subcategory </v-card-title>
+        <v-card-subtitle class="card-subtitle">
+          {{ getSubcategory(product.subCategoryId) }}
+        </v-card-subtitle>
+        <v-row>
+          <v-col cols="7">
+            <v-card-text>
+              <h6>BOBS Required:</h6>
+              <h6>SABS Required:</h6>
+              <h6>Published:</h6>
+            </v-card-text>
+          </v-col>
+          <v-col cols="5">
+            <v-card-text>
+              <h6>{{ subcatBOBS }}</h6>
+              <h6>{{ subcatSABS }}</h6>
+              <h6>{{ subcatPublished }}</h6>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+      <!-- Scraped Category -->
+      <v-card class="info-message" v-if="linkedScrapedCategory">
+        <v-card-title> Scraped Category </v-card-title>
+        <v-card-subtitle class="card-subtitle">
+          {{ linkedScrapedCategory }}
+        </v-card-subtitle>
+        <v-row>
+          <v-col cols="7">
+            <v-card-text>
+              <h6>Published:</h6>
+            </v-card-text>
+          </v-col>
+          <v-col cols="5">
+            <v-card-text>
+              <h6>{{ scrapedcatPublished }}</h6>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+      <!-- Scraped Subcategory -->
+      <v-card class="info-message" v-if="linkedScrapedSubcategory">
+        <v-card-title> Scraped Subcategory </v-card-title>
+        <v-card-subtitle class="card-subtitle">
+          {{ linkedScrapedSubcategory }}
+        </v-card-subtitle>
+        <v-row>
+          <v-col cols="7">
+            <v-card-text>
+              <h6>Published:</h6>
+            </v-card-text>
+          </v-col>
+          <v-col cols="5">
+            <v-card-text>
+              <h6>{{ scrapedSubcatPublished }}</h6>
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
     </div>
   </div>
 </template>
 
 <script>
-import productMixin from '@/mixins/productMixin.js'
+import productMixin from "@/mixins/productMixin.js";
 export default {
   props: {
-    product: null
+    product: null,
   },
   data() {
     return {
       BOBSCertificate: null,
-      SABSCertificate: null
-    }
+      SABSCertificate: null,
+      allBZCategories: [],
+      allBrands1: [],
+      thisProduct: null,
+      scrapedCategories: [],
+      linkedScrapedCategory: null,
+      linkedScrapedSubcategory: null,
+      catBOBS: null,
+      catSABS: null,
+      catPublished: null,
+      subcatBOBS: null,
+      subcatSABS: null,
+      subcatPublished: null,
+      scrapedcatPublished: null,
+      scrapedSubcatPublished: null,
+    };
   },
-  mounted() {
+  beforeMount() {
+    this.$nextTick(async function () {
+      // this.thisProduct = this.product;
+      const categories = await this.$store.dispatch("dataGate", {
+        tableName: "mappedCategories",
+        operation: "read",
+      });
 
+      this.allBZCategories = categories.data;
+
+      const scrapedCategoriesResponse = await this.$store.dispatch("dataGate", {
+        tableName: "scrapedCategories",
+        operation: "read",
+      });
+
+      this.scrapedCategories = scrapedCategoriesResponse.data;
+    });
   },
   methods: {
     async publishProduct() {
-      console.log('this.product', this.product)
+      console.log("this.product", this.product);
       if (productMixin.methods.canPublishProduct(this.product)) {
         this.product.publish = !this.product.publish;
         // Remove review required if we are publishing
@@ -103,7 +145,7 @@ export default {
         }
         await this.saveProduct();
       } else {
-        alert('You can not publish this product. Stuff is still missing.')
+        alert("You can not publish this product. Stuff is still missing.");
       }
     },
     async saveProduct() {
@@ -118,40 +160,131 @@ export default {
       const fileReader = new FileReader();
       const self = this;
       // Determine the name
-      let fileName = this.BOBSCertificate.name.split('.')[0];
-      fileName = fileName.replaceAll(' ', '');
-      fileReader.onload = async function() {
+      let fileName = this.BOBSCertificate.name.split(".")[0];
+      fileName = fileName.replaceAll(" ", "");
+      fileReader.onload = async function () {
         const response = await self.$store.dispatch("callMiddlewareRoute", {
           route: "aws/uploadFile",
           fileName,
-          contentType: 'application/' + type,
+          contentType: "application/" + type,
           file: fileReader.result,
-          folder
+          folder,
         });
         if (response) {
-          if (folder === 'BOBSCertificates') {
+          if (folder === "BOBSCertificates") {
             self.product.BOBSCertificate = response;
-          } else if (folder === 'SABSCertificates') {
+          } else if (folder === "SABSCertificates") {
             self.product.SABSCertificate = response;
           }
           await self.saveProduct();
         }
-      }
-      fileReader.readAsDataURL(this.BOBSCertificate)
+      };
+      fileReader.readAsDataURL(this.BOBSCertificate);
     },
     downloadBOBSCertificate() {
-      const link = document.createElement('a');
-      link.href = 'https://tsazonkeimages.s3.af-south-1.amazonaws.com/BOBSCertificates/test.pdf';
-      link.download = 'test.pdf';
+      const link = document.createElement("a");
+      link.href =
+        "https://tsazonkeimages.s3.af-south-1.amazonaws.com/BOBSCertificates/test.pdf";
+      link.download = "test.pdf";
       // some browser needs the anchor to be in the doc
       document.body.append(link);
       link.click();
       link.remove();
       // in case the Blob uses a lot of memory
       setTimeout(() => URL.revokeObjectURL(link.href), 7000);
-    }
-  }
-}
+    },
+    getCategory(categoryId) {
+      if (this.allBZCategories) {
+        const category = this.allBZCategories.find((x) => x.id === categoryId);
+
+        if (category) {
+          if (category.publish == 1) {
+            this.catPublished = "TRUE";
+          } else {
+            this.catPublished = "FALSE";
+          }
+
+          if (category.BOBSRequired == 1) {
+            this.catBOBS = "TRUE";
+          } else {
+            this.catBOBS = "FALSE";
+          }
+
+          if (category.SABSRequired == 1) {
+            this.catSABS = "TRUE";
+          } else {
+            this.catSABS = "FALSE";
+          }
+
+          this.linkedScrapedSubcategory = null;
+          const linkedScrapedCat = this.scrapedCategories.find(
+            (x) => x.mappedCategoryId === category.id
+          );
+          if (linkedScrapedCat) {
+            if (
+              linkedScrapedCat.parentId == null ||
+              linkedScrapedCat.parentId == "0"
+            ) {
+              this.linkedScrapedCategory = linkedScrapedCat.name;
+              if (linkedScrapedCat.publish == 1) {
+                this.scrapedcatPublished = "TRUE";
+              } else {
+                this.scrapedcatPublished = "FALSE";
+              }
+            } else {
+              const scrapedCategoryParent = this.scrapedCategories.find(
+                (x) => x.id === linkedScrapedCat.parentId
+              );
+              if (linkedScrapedCat.publish == 1) {
+                this.scrapedSubcatPublished = "TRUE";
+              } else {
+                this.scrapedSubcatPublished = "FALSE";
+              }
+              if (scrapedCategoryParent.publish == 1) {
+                this.scrapedcatPublished = "TRUE";
+              } else {
+                this.scrapedcatPublished = "FALSE";
+              }
+              this.linkedScrapedCategory = scrapedCategoryParent.name;
+              this.linkedScrapedSubcategory = linkedScrapedCat.name;
+            }
+          } else {
+            this.linkedScrapedCategory = null;
+          }
+
+          return category.name;
+        }
+      }
+      return "Category Not Found";
+    },
+    getSubcategory(categoryId) {
+      if (this.allBZCategories) {
+        const category = this.allBZCategories.find((x) => x.id === categoryId);
+        if (category) {
+          if (category.publish == 1) {
+            this.subcatPublished = "TRUE";
+          } else {
+            this.subcatPublished = "FALSE";
+          }
+
+          if (category.BOBSRequired == 1) {
+            this.subcatBOBS = "TRUE";
+          } else {
+            this.subcatBOBS = "FALSE";
+          }
+
+          if (category.SABSRequired == 1) {
+            this.subcatSABS = "TRUE";
+          } else {
+            this.subcatSABS = "FALSE";
+          }
+          return category.name;
+        }
+      }
+      return "Category Not Found";
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -159,10 +292,16 @@ export default {
   padding: 20px;
 }
 
-.info-message{
-    width: 20%;
-    float: left;
-    margin-right: 20px;
-    height: 100px;
+.info-message {
+  width: 23%;
+  float: left;
+  margin-right: 20px;
+  height: 200px;
+}
+
+.card-subtitle {
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: -10px;
 }
 </style>
