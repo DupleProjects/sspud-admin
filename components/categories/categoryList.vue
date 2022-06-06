c7d718b89a3a8610bef072a646002287caa30b11
jacqxes-du <jacques@duple.co>
2022-06-06 16:46:23 +0200

    BAM-111: Enabled subcategories to be linked to other categories when a category is deleted.

--- components/categories/categoryList.vue
+++ components/categories/categoryList.vue
@@ -92,8 +92,11 @@
     >
       <v-card>
         <v-card-title> Select New Parent Category </v-card-title>
-        <v-card-subtitle>
+        <v-card-subtitle v-if="linkedSubcategoryCount > 0">
           Select a new parent category for the {{linkedSubcategoryCount}} subcategories that are currently linked to {{categoryToDelete.name}}
+        </v-card-subtitle>
+        <v-card-subtitle v-if="linkedSubcategoryCount == 0">
+          Create a new subcategory for the products that are currently linked to {{categoryToDelete.name}} to be reassigned to.
         </v-card-subtitle>
         <v-card-text>
           <v-text-field
@@ -102,15 +105,43 @@
               :messages="['The current parent category']"
               :label="categoryToDelete.name"
           ></v-text-field>
+          <v-row>
+          <v-col cols="4">
+          <v-checkbox
+            v-model="newCheckbox"
+            :disabled="disableNew"
+            v-on:change="selectField('new')"
+            label="Create New Subcategory:"
+          ></v-checkbox>
+          </v-col>
+          <v-col cols="4">
+          <v-checkbox
+            v-model="existingCheckbox"
+            :disabled="disableExisting"
+            v-on:change="selectField('existing')"
+            label="Select Existing Subcategory:"
+          ></v-checkbox>
+          </v-col>
+          </v-row>
           <v-autocomplete
+              v-if="existingCheckbox == true"
               prepend-icon="mdi-clipboard-check-multiple"
               label="Parent"
+              required
               :item-text="'name'"
               :item-value="'id'"
               :items="availableCategories"
               v-model="newParentId"
               :messages="['Choose a new parent category']"
           ></v-autocomplete>
+          <v-text-field
+              v-if="newCheckbox == true"
+              v-model="newCreatedParentName"
+              required
+              prepend-icon="mdi-card-account-details-outline"
+              :messages="['Create a new subcategory for the products that are currently linked to the current subcategory to be reassigned to.']"
+              label="Create a new category"
+          ></v-text-field>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -145,12 +176,18 @@ export default {
       linkedSubcategoryCount: 0,
       newParentId: null,
       confirmedNewParentId: null,
+      confirmedNewCategoryName: null,
       categoryToDelete: {},
-      availableCategories: []
+      availableCategories: [],
+      newAssignedCategory: false,
+      newCreatedParentName: null,
+      disableExisting: false,
+      disableNew: false,
+      newCheckbox: false,
+      existingCheckbox:false
     }
   },
   mounted() {
-    console.log("🔥",this.categories);
   },
   methods: {
     goToCategoryDashboard(category) {
@@ -173,68 +210,137 @@ export default {
       return 'No parent category'
     },
     checkForLinkedSubcategories(category){
-      console.log("🙌🙌", this.allCategories);
-      // console.log("1. CHECK LINKED SUBCATEGORIES");
       this.categoryToDelete = category;
       var counter = 0;
-      this.allCategories.forEach(element => {
-        // console.log(element);
-        if (element.parentId === category.id) {
-          console.log(element.name);
-          counter ++;
-        }
-        if (element.id != category.id) {
-          if (element.parentId === 0 || element.parentId == null) {
-            this.availableCategories.push(element);
-          }
-        }
-        
-      });
+      this.availableCategories = [];
 
-        if(counter >= 1){
-          console.log("2. OPEN FORM");
-          this.linkedSubcategoryCount = counter;
+      if(category.parentId != 0 && category.parentId != null){
+          this.allCategories.forEach(element => {
+              if (element.parentId == category.parentId && element.id != category.id) {
+                this.availableCategories.push(element);
+              }
+          });
+
+          if(this.availableCategories.length == 0){
+            this.existingCheckbox = false;
+            this.newCheckbox = true;
+            this.disableExisting = true;
+            this.disableNew = true;
+            this.newCreatedParentName = null;
+          }else{
+            this.existingCheckbox = true;
+            this.newCheckbox = false;
+            this.disableExisting = true;
+            this.disableNew = false;
+            this.newParentId = null;
+          }
+
           this.linkCategoriesDialog = true;
-        }else{
-          this.openTheDeleteDialog()
-        }
+          this.newAssignedCategory = true;
+          this.linkedSubcategoryCount = 0;
+          
+          this.newParentId = null
+          this.newCreatedParentName = null
+
+
+      }else{
+        this.newAssignedCategory = false;
+          this.allCategories.forEach(element => {
+            if (element.parentId === category.id) {
+              counter ++;
+            }
+            if (element.id != category.id) {
+              if (element.parentId === 0 || element.parentId == null) {
+                this.availableCategories.push(element);
+              }
+            }
+          });
+
+          if(this.availableCategories.length == 0){
+            this.existingCheckbox = false;
+            this.newCheckbox = true;
+            this.disableExisting = true;
+            this.disableNew = true;
+            this.newCreatedParentName = null;
+          }else{
+            this.existingCheckbox = true;
+            this.newCheckbox = false;
+            this.disableExisting = true;
+            this.disableNew = false;
+            this.newParentId = null;
+          }
+
+          if(counter >= 1){
+            this.linkedSubcategoryCount = counter;
+            this.linkCategoriesDialog = true;
+          }else{
+            this.openTheDeleteDialog()
+          }
+      }
+      
 
     },
     openTheDeleteDialog(category) {
-      console.log("3. OPEN DELETE DIALOG");
       if(category) {
         this.categoryToDelete = category;
       }
-      // console.log("🔥🔥On Click", this.categoryToDelete);
       this.deleteDialog = true;
     },
     closeTheDeleteDialog() {
       this.deleteDialog = false;
+      this.confirmedNewParentId = null;
+      this.confirmedNewCategoryName = null;
       this.categoryToDelete = {};
     },
     closeLinkCategoriesDialog() {
-      console.log("3. CLOSE LINKED FORM");
       this.linkCategoriesDialog = false;
       // this.categoryToDelete = {};
     },
     setNewParentId(){
-      this.confirmedNewParentId = this.newParentId;
+      if(this.newParentId){
+        this.confirmedNewParentId = this.newParentId;
+      }else{
+        this.confirmedNewParentId = null;
+      }
+      if(this.newCreatedParentName){
+        this.confirmedNewCategoryName = this.newCreatedParentName;
+      }else{
+        this.confirmedNewCategoryName = null;
+      }
+
+      console.log("👉Cat to delete",this.categoryToDelete);
+      console.log("👉👉New parentId",this.confirmedNewParentId);
+      console.log("👉👉👉New category name",this.confirmedNewCategoryName);
+
       this.linkCategoriesDialog = false;
       this.deleteDialog = true;
     },
     async deleteCategory() {
-      console.log("🔥🔥🔥🔥CAT TO DELETE",this.categoryToDelete.id);
-      console.log("🔥🔥🔥🔥NEW PARENT ID",this.confirmedNewParentId);
       // delete the product from scrapedProducts
       const deleteResponse = await this.$store.dispatch("callMiddlewareRoute", {
         category: this.categoryToDelete,
         route: 'categories/deleteCategory',
-        newParentCategory: this.confirmedNewParentId
+        newParentCategory: this.confirmedNewParentId,
+        newCategoryName: this.confirmedNewCategoryName
       });
 
       this.deleteDialog = false;
       this.closeTheDeleteDialog();
     },
+    selectField(checkbox){
+      if(checkbox == "new"){
+        this.existingCheckbox = false;
+        this.disableNew = true;
+        this.disableExisting = false;
+        this.newParentId = null;
+      }
+      if(checkbox == "existing"){
+        this.newCheckbox = false;
+        this.disableNew = false;
+        this.disableExisting = true;
+        this.newCreatedParentName = null;
+      }
+    }
   }
 }
 </script>
