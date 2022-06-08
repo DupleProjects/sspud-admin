665e2c56162f3ef6e483bb169662bcc60da0800a
jacqxes-du <jacques@duple.co>
2022-06-08 13:16:39 +0200

    Resolved bug on category dashboard where the  parent categories did not display correctly in the linked categories component

--- pages/categories/dashboard/index.vue
+++ pages/categories/dashboard/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
+    <div
+      class="
+        d-flex
+        justify-content-between
+        flex-wrap flex-md-nowrap
+        align-items-center
+        pt-3
+        pb-2
+        mb-3
+        border-bottom
+      "
+    >
     <h2>Category Dashboard</h2>
-    <hr>
+    </div>
 
   </div>
 </template>
