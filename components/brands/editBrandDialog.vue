633ee00ff5a0cc3f49d15406261983234b212192
MarkusAfricanus <marco.pieterse@gmail.com>
2022-06-12 17:49:36 +0200

    Improved product page

--- components/brands/editBrandDialog.vue
+++ components/brands/editBrandDialog.vue
@@ -86,7 +86,6 @@ export default {
       // Remove quotes
       this.brand.name = this.brand.name.replaceAll("'", "");
       this.brand.name = this.brand.name.replaceAll('"', "");
-      console.log('this.brand.name', this.brand.name)
       this.loading = true;
       // Save the  category if the form is valid
       if (this.$refs.newCategoryForm.validate()) {
